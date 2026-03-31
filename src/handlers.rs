e5a1076f4cea09e6b2a2a63df41f207c3afa5c12
seika-afk <artistnoob6@gmail.com>
2026-04-01 00:23:20 +0530

    added base routing

---
+++ src/handlers.rs
@@ -0,0 +1,13 @@
+use axum::{Json,response::IntoResponse};
+use serde_json::{json};
+
+pub async fn hello_world()->impl IntoResponse{
+
+let json_response= json!({
+
+    "status" : "ok",
+    "message" : "Hello_world"
+
+});
+    Json(json_response)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Json,Router,response::IntoResponse,routing::get };
+use serde_json::json;
+
+
+mod handlers;
+use handlers::hello_world;
+
+
+#[tokio::main]
+async fn main()->Result<(),Box<dyn std::error::Error>>{
+
+        let app = Router::new().route("/api", get(hello_world));
+        let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+        println!("Server Started : 0.0.0.0:3000 ");
+
+        axum::serve(listener, app).await?;
+
+
+Ok(())
 }
+
+
+
